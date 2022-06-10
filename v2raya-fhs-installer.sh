4b0ecb89efc05a55b2c2b2f9282a5bbba7708553
Markson Hon <markson@2333ma.top>
2022-06-10 15:52:14 +0800

    Update

--- v2raya-fhs-installer.sh
+++ v2raya-fhs-installer.sh
@@ -1,18 +1,21 @@
-#! /bin/bash
+#! /bin/sh
 
 GitHub_API_URL="https://api.github.com/repos/v2rayA/v2rayA/releases/latest"
 GitHub_Release_URL="https://github.com/v2rayA/v2rayA/releases"
 
+#1. Check Latest Version
 CheckLatestVersion(){
-    LatestVersion=$(curl -s $GitHub_API_URL | jq '.tag_name' | cut -d'"' -f2 | cut -b 2-10)
+    LatestVersion=$(curl -s $GitHub_API_URL | jq '.tag_name' | cut -d '"' -f2 | cut -b 2-10)
     echo "Latest Version is $LatestVersion"
 }
 
-DownloadUrl(){
+#2. GetUrl
+GetUrl(){
     DownloadUrlx64="$GitHub_Release_URL/download/v$LatestVersion/v2raya_linux_x64_$LatestVersion"
     DownloadUrlarm64="$GitHub_Release_URL/download/v$LatestVersion/v2raya_linux_arm64_$LatestVersion"
 }
 
+#3. Check Current Version
 CheckCurrentVersion(){
     if [ ! -f /usr/local/bin/v2raya ]; then
     CurrentVersion="none"
@@ -22,7 +25,10 @@ CheckCurrentVersion(){
     echo "Current Version is $CurrentVersion"
 }
 
+#4-1 Make SystemD Service
 MakeSystemDService(){
+    if [ ! -f /etc/systemd/system/v2raya.service ]; then
+    echo "Making '/etc/systemd/system/v2raya.service'"
     echo "
 [Unit]
 Description=v2rayA Service
@@ -42,55 +48,112 @@ Restart=on-failure
 
 [Install]
 WantedBy=multi-user.target" > /etc/systemd/system/v2raya.service
+fi
+if [ ! -d "/etc/systemd/system/v2raya.service.d/" ]; then
+    echo "Marking '/etc/systemd/system/v2raya.service.d/'"
+    mkdir -p /etc/systemd/system/v2raya.service.d/
+fi
 }
 
+#4-2 Make OpenRC Service
+MakeOpenRCService(){
+    if [ ! -f /etc/init.d/v2raya ]; then
+    echo "
+#\!/sbin/openrc-run
+
+name=\"v2rayA\"
+description=\"A Linux web GUI client of Project V which supports V2Ray, Xray, SS, SSR, Trojan and Pingtunnel\"
+
+command=\"/usr/local/bin/v2raya\"
+command_args=\"--log-file /var/log/v2raya/access.log\"
+error_log=\"/var/log/v2raya/error.log\"
+pidfile=\"/run/${RC_SVCNAME}.pid\"
+command_background=\"yes\"
+rc_ulimit=\"-n 30000\"
+rc_cgroup_cleanup=\"yes\"
+
+depend() {
+    need net
+    after net
+}
+
+start_pre() {
+   export V2RAYA_CONFIG="/usr/local/etc/v2raya"
+   if [ ! -d \"/tmp/v2raya/\" ]; then 
+     mkdir \"/tmp/v2raya\" 
+   fi
+   if [ ! -d \"/var/log/v2raya/\" ]; then
+   ln -s \"/tmp/v2raya/\" \"/var/log/\"
+   fi
+}" > '/etc/init.d/v2raya'
+fi
+}
+
+#5. Get System Information
 GetSystemInformation(){
     SystemType=$(uname)
     SystemArch=$(uname -m)
 }
 
-v2rayAInstallation(){
+#6. Download v2rayA
+Download_v2rayA(){
     if [ $SystemArch == x86_64 ];then
     curl -L $DownloadUrlx64 -o "/tmp/v2raya"
     fi
     if [ $SystemArch == aarch64 ];then
     curl -L $DownloadUrlarm64 -o "/tmp/v2raya"
     fi
+    if [ $SystemArch != x86_64 && $SystemArch != aarch64 ];then
+    echo "You have an unsupported system architecture, script will exit now!"
+    exit 9
+    fi
+}
+
+StopService(){
     if [ -f /etc/systemd/system/v2raya.service ];then
     echo "Stopping v2raya"
     systemctl stop v2raya
-    v2rayAServiceStopped=true
+    v2rayAServiceStopped=1
     fi
-    if [[ -n $(ps -ef | grep v2raya | gawk '$0 !~/grep/ {print $2}' |tr -s '\n' ' ') ]];then
-    kill -15 $(pidof v2raya)
+    if [ -f /etc/init.d/v2raya ]; then
+    echo "Stopping v2raya"
+    rc-service stop v2raya
+    v2rayAServiceStopped=1
     fi
-    mv /tmp/v2raya /usr/local/bin/v2raya
-    chmod 555 /usr/local/bin/v2raya
+}
+
+StartService(){
+    if [ v2rayAServiceStopped == 1 && -f /etc/systemd/system/v2raya.service ];then
+    systemctl start v2raya
+    fi
+    if [ v2rayAServiceStopped == 1 && -f /etc/init.d/v2raya ]; then
+    rc-service v2raya start
+}
+
+InstallService(){
+    if [ -f /sbin/openrc-run ]; then
+    MakeOpenRCService
+    echo "If you want to start v2rayA at system startup, please run:"
+    echo "rc-update add v2raya"
+    fi
+    if [ -f /usr/lib/systemd/systemd ]; then
+    MakeSystemDService
+    echo "If you want to start v2rayA at system startup, please run:"
+    echo "systemctl enable v2raya"
+    fi
 }
 
 main(){
     CheckCurrentVersion
     CheckLatestVersion
     if [ $CurrentVersion != $LatestVersion ];then
-        GetSystemInformation
-        DownloadUrl
-        v2rayAInstallation
-        echo "v2rayA new version installed, use systemctl to start, stop or restart it."
-        echo " use \"systemctl enable v2raya\" to make v2rayA start at system startup."
-        else
-        echo "There is no update for v2rayA, you have latest version."
-    fi
-    if [ ! -f /etc/systemd/system/v2raya.service ];then
-        echo "Making '/etc/systemd/system/v2raya.service'"
-        MakeSystemDService
-    fi    
-    if [ ! -d "/etc/systemd/system/v2raya.service.d/" ]; then
-        echo "Marking '/etc/systemd/system/v2raya.service.d/'"
-        mkdir -p /etc/systemd/system/v2raya.service.d/
-    fi
-    if [ $v2rayAServiceStopped == "true" ]; then
-    echo "Starting v2raya"
-    systemctl start v2raya
+    GetSystemInformation
+    GetUrl
+    Download_v2rayA
+    StopService
+    cp "/tmp/v2raya" "/usr/local/bin/v2raya"
+    InstallService
+    StartService
     fi
 }
 

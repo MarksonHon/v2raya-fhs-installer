b0abc431a5d2d2ccbd1332e3222659e8c2d1fce7
markson <markson@2333ma.top>
2022-08-18 21:30:41 +0800

    We lost OSDN

--- v2raya-fhs-installer.sh
+++ v2raya-fhs-installer.sh
@@ -2,7 +2,7 @@
 
 GitHub_API_URL="https://api.github.com/repos/v2rayA/v2rayA/releases/latest"
 GitHub_Release_URL="https://github.com/v2rayA/v2rayA/releases"
-OSDN_Mirror_URL="https://zh.osdn.net/projects/v2raya/storage/releases"
+# OSDN_Mirror_URL="https://zh.osdn.net/projects/v2raya/storage/releases"
 
 CheckLatestVersion(){
     LatestVersion=$(curl -s $GitHub_API_URL | jq '.tag_name' | cut -d '"' -f2 | cut -b 2-16)
@@ -10,8 +10,8 @@ CheckLatestVersion(){
 }
 
 GetUrl(){
-    DownloadUrlx64="$OSDN_Mirror_URL/v$LatestVersion/v2raya_linux_x64_$LatestVersion"
-    DownloadUrlarm64="$OSDN_Mirror_URL/v$LatestVersion/v2raya_linux_arm64_$LatestVersion"
+    DownloadUrlx64="$GitHub_Release_URL/download/v$LatestVersion/v2raya_linux_x64_$LatestVersion"
+    DownloadUrlarm64="$GitHub_Release_URL/download/v$LatestVersion/v2raya_linux_arm64_$LatestVersion"
 }
 
 CheckCurrentVersion(){
@@ -102,22 +102,22 @@ Download_v2rayA(){
     if [ $SystemArch == x86_64 ];then
     echo -e "\033[42;37m Downloading v2rayA... \033[0m"
     curl -Ls $DownloadUrlx64 -o "/tmp/v2raya_temp"
-    Sha256_x64_local=$(sha256sum "/tmp/v2raya_temp" | cut -d ' ' -f1)
-    Sha256_x64_Online=$(curl -sL https://zh.osdn.net/projects/v2raya/storage/releases/v$LatestVersion/info.json | grep "v2raya_linux_x64_$LatestVersion" |  cut -d '"' -f4)
-        if [ $Sha256_x64_local != $Sha256_x64_Online ]; then
-        echo -e "\033[41;37m Error: Sha256 NOT match! Please check your network and try again. \033[0m"
-        exit 1
-        fi
-    fi
+    # Sha256_x64_local=$(sha256sum "/tmp/v2raya_temp" | cut -d ' ' -f1)
+    # Sha256_x64_Online=$(curl -sL https://zh.osdn.net/projects/v2raya/storage/releases/v$LatestVersion/info.json | grep "v2raya_linux_x64_$LatestVersion" |  cut -d '"' -f4)
+    #     if [ $Sha256_x64_local != $Sha256_x64_Online ]; then
+    #     echo -e "\033[41;37m Error: Sha256 NOT match! Please check your network and try again. \033[0m"
+    #     exit 1
+    #     fi
+    # fi
     if [ $SystemArch == aarch64 ];then
     echo -e "\033[42;37m Downloading v2rayA... \033[0m"
     curl -Ls $DownloadUrlarm64 -o "/tmp/v2raya_temp"
-    Sha256_arm64_local=$(sha256sum "/tmp/v2raya_temp" | cut -d ' ' -f1)
-    Sha256_arm64_Online=$(curl -sL https://zh.osdn.net/projects/v2raya/storage/releases/v$LatestVersion/info.json | grep "v2raya_linux_arm64_$LatestVersion" |  cut -d '"' -f4)
-        if [ $Sha256_arm64_local != $Sha256_arm64_Online ]; then
-        echo -e "\033[41;37m Error: Sha256 NOT match! Please check your network and try again. \033[0m"
-        exit 1
-        fi
+    # Sha256_arm64_local=$(sha256sum "/tmp/v2raya_temp" | cut -d ' ' -f1)
+    # Sha256_arm64_Online=$(curl -sL https://zh.osdn.net/projects/v2raya/storage/releases/v$LatestVersion/info.json | grep "v2raya_linux_arm64_$LatestVersion" |  cut -d '"' -f4)
+        # if [ $Sha256_arm64_local != $Sha256_arm64_Online ]; then
+        # echo -e "\033[41;37m Error: Sha256 NOT match! Please check your network and try again. \033[0m"
+        # exit 1
+        # fi
     fi
     if [ $SystemArch != x86_64 ] && [ $SystemArch != aarch64 ];then
     echo "You have an unsupported system architecture, script will exit now!"
